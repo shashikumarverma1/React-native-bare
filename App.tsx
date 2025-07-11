72810abfcaf26ef6c4c3156bdfeae22b963f3f29
shashi kumar verma <shashikumarverma1996@gmail.com>
2025-07-11 16:40:50 +0530

    re

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ console.log("wwowo" , 999)
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Text style={{padding:50}}>wowo</Text>
+      
     </View>
   );
 }
